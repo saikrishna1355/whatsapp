ddcf14a204f81b12416c684a4d7e255256fe7a89
Sai Krishna M <m.saikrishna@novactech>
2026-05-14 14:58:06 +0530

    initial commits

--- src/app.js
+++ src/app.js
@@ -2,24 +2,26 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Webhook Running");
+  res.send("Server Running");
 });
 
 app.get("/webhook", (req, res) => {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "myverifytoken";
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  console.log("MODE:", mode);
-  console.log("TOKEN:", token);
-  console.log("CHALLENGE:", challenge);
+  console.log({
+    mode,
+    token,
+    challenge,
+    VERIFY_TOKEN,
+  });
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
     console.log("WEBHOOK VERIFIED");
@@ -30,11 +32,14 @@ app.get("/webhook", (req, res) => {
   return res.sendStatus(403);
 });
 
-app.post("/webhook", async (req, res) => {
+app.post("/webhook", (req, res) => {
+  console.log("Incoming webhook:");
   console.log(JSON.stringify(req.body, null, 2));
 
-  res.sendStatus(200);
+  return res.sendStatus(200);
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
